3e3e51a7f0c7a2be409c174d9ea477a891321cce
Aline Vernier <aline.vernier@polytechnique.edu>
2025-12-17 12:51:30 +0100

    Started to implement controls and indicators. Still to be done: implement stat bounds properly, consider putting an utils module to make class lighter and easier to read...

--- visu/winSpectro2.py
+++ visu/winSpectro2.py
@@ -6,7 +6,7 @@ Created on 2025/12/16
 Spectrum deconvolution + make data available
 """
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QHBoxLayout, QGridLayout
-from PyQt6.QtWidgets import QLabel, QMainWindow, QFileDialog,QStatusBar, QCheckBox, QDoubleSpinBox
+from PyQt6.QtWidgets import QLabel, QMainWindow, QFileDialog,QStatusBar, QCheckBox, QDoubleSpinBox, QSlider
 from PyQt6 import QtCore, QtGui
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction
@@ -49,6 +49,7 @@ class WINSPECTRO(QMainWindow):
 
         # Main window setup
         self.setup()
+        self.action_button()
 
         # Load calibration data
         self.load_calib()
@@ -58,7 +59,7 @@ class WINSPECTRO(QMainWindow):
     def setup(self):
 
         #####################################################################
-        # Window setup
+        #                   Window setup
         #####################################################################
         self.isWinOpen = False
         self.setWindowTitle('Electrons spectrometer')
@@ -75,7 +76,7 @@ class WINSPECTRO(QMainWindow):
         self.fileMenu = self.menuBar().addMenu('&File')
 
         #####################################################################
-        # Global layout and geometry
+        #                   Global layout and geometry
         #####################################################################
 
         # Horizontal box with LHS graphs, and RHS controls and indicators
@@ -92,16 +93,27 @@ class WINSPECTRO(QMainWindow):
         self.vbox2 = QVBoxLayout()
         self.vbox2widget = QWidget()
         self.vbox2widget.setLayout(self.vbox2)
-        #self.vbox2widget.setFixedWidth(300)
+        self.vbox2widget.setFixedWidth(300)
         self.hbox.addWidget(self.vbox2widget)
+
+        # Title
+        title_layout = QGridLayout()
+        self.vbox2.addLayout(title_layout)
+        Title = QLabel('Controls and indicators')
+        ph_1 = QLabel()
+        ph_2 = QLabel()
+        title_layout.addWidget(ph_1, 0, 0)
+        title_layout.addWidget(Title, 0, 1)
+        title_layout.addWidget(ph_2, 0, 2)
 
         # Grid layout for controls and indicators
         self.grid_layout = QGridLayout()
+
         self.vbox2.addLayout(self.grid_layout)  # add grid to RHS panel
         self.vbox2.addStretch(1)
 
         #####################################################################
-        # Fill layout with graphs, controls and indicators
+        #       Fill layout with graphs, controls and indicators
         #####################################################################
 
         # 2D plot (image histogram) in LHS vbox
@@ -127,30 +139,83 @@ class WINSPECTRO(QMainWindow):
         self.dnde_image.setContentsMargins(10, 10, 10, 10)
 
         # Controls and indicators, labels
-        Title = QLabel('Controls and indicators')
-        self.flip_image = QCheckBox('Deflection: Right to Left?', self)
+
+        self.flip_image = QCheckBox('Deflect.: R to L?', self)
         self.flip_image.setChecked(True)
 
         cutoff_energies = QLabel('Cutoff energies (MeV)')
-        self.min_cutoff_energy = QDoubleSpinBox()  # for the value
-        self.min_cutoff_energy.setValue(10)
-        self.min_cutoff_energy.setMinimum(0)
-        self.min_cutoff_energy.setSingleStep(1)
+        self.min_cutoff_energy_control = QDoubleSpinBox()  # for the value
+        self.min_cutoff_energy_control.setValue(10)
+        self.min_cutoff_energy_control.setMinimum(0)
+        self.min_cutoff_energy_control.setSingleStep(1)
 
-        self.max_cutoff_energy = QDoubleSpinBox()  # for the value
-        self.max_cutoff_energy.setValue(200)
-        self.max_cutoff_energy.setMinimum(50)
-        self.max_cutoff_energy.setSingleStep(10)
+        self.max_cutoff_energy_control = QDoubleSpinBox()  # for the value
+        self.max_cutoff_energy_control.setValue(200)
+        self.max_cutoff_energy_control.setMinimum(50)
+        self.max_cutoff_energy_control.setSingleStep(10)
 
         # Fill grid with controls and indicators
-        self.grid_layout.addWidget(Title,0,1)
+
         self.grid_layout.addWidget(QLabel(), 1, 3)
         self.grid_layout.addWidget(self.flip_image, 2, 0)
         self.grid_layout.addWidget(cutoff_energies, 3, 0)
-        self.grid_layout.addWidget(self.min_cutoff_energy, 3, 1)
-        self.grid_layout.addWidget(self.max_cutoff_energy, 3, 2)
+        self.grid_layout.addWidget(self.min_cutoff_energy_control, 3, 1)
+        self.grid_layout.addWidget(self.max_cutoff_energy_control, 3, 2)
 
+        # Brightness
+        grid_layout_brightness = QGridLayout()
+        grid_layout_brightness.addWidget(QLabel(), 0, 0)  # skip a line
+        brightnessLabel = QLabel("Brightness:")
+        grid_layout_brightness.addWidget(brightnessLabel, 1, 0)
+        self.brightnessSlider = QSlider(Qt.Orientation.Horizontal)
+        self.brightnessSlider.setRange(0, 100)
+        self.brightnessSlider.setValue(50)
+        grid_layout_brightness.addWidget(self.brightnessSlider, 2, 0)
+        self.brightnessBox = QDoubleSpinBox()
+        self.brightnessBox.setRange(0, 100)
+        self.brightnessBox.setValue(self.brightnessSlider.value())
+        grid_layout_brightness.addWidget(self.brightnessBox, 2, 1)
+        self.vbox2.addLayout(grid_layout_brightness)
 
+    #####################################################################
+    #                       Interface actions
+    #####################################################################
+
+    def action_button(self):
+        self.brightnessSlider.valueChanged.connect(self.updateBrightness)
+        self.brightnessBox.valueChanged.connect(self.updateBrightnessBox)
+        self.min_cutoff_energy_control.valueChanged.connect(self.change_energy_bounds)
+        self.max_cutoff_energy_control.valueChanged.connect(self.change_energy_bounds)
+
+    def change_energy_bounds(self):
+        self.min_cutoff_energy = self.min_cutoff_energy_control.value()
+        self.max_cutoff_energy = self.max_cutoff_energy_control.value()
+
+    def updateBrightness(self, val):
+        self.brightnessBox.setValue(self.brightnessSlider.value())
+        # brightness levels
+        levels = self.image_histogram.getLevels()
+        if levels[0] is None:
+            self.base_xmin = float(self.data.min())
+            self.base_xmax = float(self.data.max())
+        else:
+            self.base_xmin = float(levels[0])
+            self.base_xmax = float(levels[1])
+        xmin = self.base_xmin
+
+        span = self.base_xmax - self.base_xmin
+        factor = (val - 50) / 50.0
+        xmax = self.base_xmax - factor * span
+
+        if xmax <= xmin:
+            xmax = xmin + 1e-9
+
+        self.image_histogram.setLevels([xmin, xmax])
+        self.hist.setHistogramRange(xmin, xmax)
+
+    def updateBrightnessBox(self):
+        self.brightnessSlider.setValue(int(self.brightnessBox.value()))
+        self.updateBrightness(self.brightnessBox.value())
 
     def load_calib(self):
         # Load calibration for spectrum deconvolution
@@ -198,8 +263,7 @@ class WINSPECTRO(QMainWindow):
         # Integrate over angle and show graph
         self.deconvolved_spectrum.integrate_spectrum((600, 670), (750, 850))
         self.dnde_image.plot(self.deconvolved_spectrum.energy, self.deconvolved_spectrum.integrated_spectrum)
-
-
+        self.updateBrightness(self.brightnessBox.value())
 
     def spectro_dict(self, temp_dataArray):
         # Creation of dictionary to pass to diagServ ; to be integrated to another function, with another signal
